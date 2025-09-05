638bff19ae1cc74a0622b65c4fe78fdb41e5b1cb
Landon Borrego <lndnborrrego@gmail.com>
2025-09-05 13:49:24 -0400

    workout data by muscle group appears for a specific user

--- src/app/pages/muscleGroups/[id]/page.js
+++ src/app/pages/muscleGroups/[id]/page.js
@@ -1,12 +1,29 @@
+'use client';
+
 import PropTypes from 'prop-types';
+import { getMgWorkouts } from '@/api/workoutAPI';
+import { useAuth } from '@/utils/context/authContext';
+import { useEffect, useState } from 'react';
+import WorkoutLog from '@/components/cards/WorkoutLog';
 
 function WorkoutsByMG({ params }) {
+  const [workouts, setWorkouts] = useState([]);
+  const { user } = useAuth();
   const { id } = params;
+
+  useEffect(() => {
+    getMgWorkouts(user.uid, id).then(setWorkouts);
+  }, [user.uid, id]);
+
+  console.log('current state of workouts', workouts);
 
   return (
     <>
       <h3>This is the page where we see all Workouts by Muscle Group!</h3>
       <p>This is the id: {id}</p>
+      {workouts.map((wo) => (
+        <WorkoutLog key={wo.id} workoutObj={wo} />
+      ))}
     </>
   );
 }
