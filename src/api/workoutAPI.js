aee1c03d891e0be4526c713d569d31b43a2f60aa
Landon Borrego <lndnborrrego@gmail.com>
2025-09-12 22:35:10 -0400

    Update functionality is successful for workout.

--- src/api/workoutAPI.js
+++ src/api/workoutAPI.js
@@ -40,4 +40,31 @@ const createWorkout = (payload) =>
       .catch(reject);
   });
 
-export { getUidWorkouts, getMgWorkouts, createWorkout };
+const updateWorkout = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`http://localhost:8000/workouts/${payload.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const getSingleWorkout = (id) =>
+  new Promise((resolve, reject) => {
+    fetch(`http://localhost:8000/workouts/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+export { getUidWorkouts, getMgWorkouts, createWorkout, updateWorkout, getSingleWorkout };
